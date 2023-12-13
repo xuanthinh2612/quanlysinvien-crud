a0b2e2361d8d677d8c1d6df96f18ef15821e988c
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-12-13 23:05:39 +0900

    update deploy file

--- stop_server.sh
+++ stop_server.sh
@@ -1,8 +1,13 @@
-#cd ~
-#export CATALINA_HOME="/opt/tomcat"
-#export PATH="$PATH:$CATALINA_HOME/bin"
-#source ~/.bashrc
-#catalina.sh stop
+cd ~
+export CATALINA_HOME="/opt/tomcat"
+export PATH="$PATH:$CATALINA_HOME/bin"
+source ~/.bashrc
+catalina.sh stop
 
 #sudo killall java
 #exit 0
+
+rm -r /opt/tomcat/webapps/ROOT
+rm /opt/tomcat/webapps/ROOT.war
+
+cp ROOT.war /opt/tomcat/webapps
