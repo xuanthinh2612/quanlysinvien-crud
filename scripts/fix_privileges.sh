b159a1fdb254ab95027ef8aee588272eaca75d30
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-12-14 23:51:01 +0900

    test-pipeline

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+chmod +x /home/ubuntu/server/* jar
+chmod +x /home/ubuntu/server/* war
+chmod +x /home/ubuntu/server/start_server.sh
+chmod +x /home/ubuntu/server/stop_server.sh
