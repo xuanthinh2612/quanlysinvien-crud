49b3ec6f864babda3c2eaa3a172978c831ca1598
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-12-16 01:30:04 +0900

    test-pipeline

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -9,20 +9,20 @@
 
 #!/bin/bash
 
-service_name="rebootServer.service"
-service_path="/etc/systemd/system/$service_name"
-original_service_path="/server/scripts/$service_name"
-
-# Kiểm tra xem dịch vụ đã tồn tại hay chưa
-if systemctl list-units --full --all | grep -Fq "$service_name"; then
-    echo "Dịch vụ $service_name đã tồn tại. Không cần thêm mới."
-else
-    # Copy tệp .service vào thư mục systemd và làm mới systemd
-    cp "$original_service_path" "$service_path"
-    systemctl daemon-reload
-
-    echo "Đã thêm mới dịch vụ $service_name và làm mới systemd."
-fi
+#service_name="rebootServer.service"
+#service_path="/etc/systemd/system/$service_name"
+#original_service_path="/server/scripts/$service_name"
+#
+## Kiểm tra xem dịch vụ đã tồn tại hay chưa
+#if systemctl list-units --full --all | grep -Fq "$service_name"; then
+#    echo "Dịch vụ $service_name đã tồn tại. Không cần thêm mới."
+#else
+#    # Copy tệp .service vào thư mục systemd và làm mới systemd
+#    cp "$original_service_path" "$service_path"
+#    systemctl daemon-reload
+#
+#    echo "Đã thêm mới dịch vụ $service_name và làm mới systemd."
+#fi
 
 
 #Để xóa service_name khỏi danh sách dịch vụ trên systemd, bạn có thể sử dụng các bước sau:
@@ -50,3 +50,8 @@ fi
 # systemctl list-units --full --all | grep -Fq "$service_name"
 # echo $?
 # Kết quả của $? sẽ là mã thoát (exit code) của lệnh trước đó. Nếu nó là 0, đó có nghĩa là grep đã tìm thấy kết quả.
+
+#cach tao service (file.service)
+#    # Copy tệp .service vào thư mục systemd và làm mới systemd
+#    cp "$original_service_path" "$service_path"
+#    systemctl daemon-reload
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#sudo systemctl stop rebootServer
+sudo systemctl stop rebootServer
 
 #
-service_name="rebootServer.service"
-service_path="/etc/systemd/system/$service_name"
-original_service_path="/server/scripts/$service_name"
-
-# Kiểm tra xem dịch vụ đã tồn tại hay chưa
-if systemctl list-units --full --all | grep -Fq "$service_name"; then
-  #  nếu có service rồi thì sẽ stop service
-  sudo systemctl stop rebootServer
-  echo "đã stop service $service_name"
-else
-# Kiểm tra xem có quy trình Java đang chạy không
-  if pgrep -x "java" > /dev/null
-  then
-    echo "Đang có quy trình Java chạy. Đang tiêu diệt..."
-    sudo killall java
-    echo "Đã tiêu diệt quy trình Java."
-  else
-    echo "Không có quy trình Java đang chạy."
-  fi
-fi
+#service_name="rebootServer.service"
+#service_path="/etc/systemd/system/$service_name"
+#original_service_path="/server/scripts/$service_name"
+#
+## Kiểm tra xem dịch vụ đã tồn tại hay chưa
+#if systemctl list-units --full --all | grep -Fq "$service_name"; then
+#  #  nếu có service rồi thì sẽ stop service
+#  sudo systemctl stop rebootServer
+#  echo "đã stop service $service_name"
+#else
+## Kiểm tra xem có quy trình Java đang chạy không
+#  if pgrep -x "java" > /dev/null
+#  then
+#    echo "Đang có quy trình Java chạy. Đang tiêu diệt..."
+#    sudo killall java
+#    echo "Đã tiêu diệt quy trình Java."
+#  else
+#    echo "Không có quy trình Java đang chạy."
+#  fi
+#fi
 
 
 
