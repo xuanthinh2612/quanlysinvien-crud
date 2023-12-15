054d1ffa0a49b611e24196e556386e157b8361f9
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-12-15 19:26:29 +0900

    test-pipeline

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,12 @@
-export CATALINA_HOME="/opt/tomcat"
-export PATH="$PATH:$CATALINA_HOME/bin"
-source ~/.bashrc
-catalina.sh stop
-
-#sudo killall java
-#exit 0
-
-#rm -r /opt/tomcat/webapps/ROOT
-#rm /opt/tomcat/webapps/ROOT.war
-
-cp server/ROOT.war /opt/tomcat/webapps
+#export CATALINA_HOME="/opt/tomcat"
+#export PATH="$PATH:$CATALINA_HOME/bin"
+#source ~/.bashrc
+#catalina.sh stop
+#
+##sudo killall java
+##exit 0
+#
+##rm -r /opt/tomcat/webapps/ROOT
+##rm /opt/tomcat/webapps/ROOT.war
+#
+#cp server/ROOT.war /opt/tomcat/webapps
