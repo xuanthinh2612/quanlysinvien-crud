f4705be56beeed93019bec4f3c17f31bc66f59d2
Tran Xuan Thinh <xuanthinh2612@gmail.com>
2023-12-15 22:08:18 +0900

    test-pipeline

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -3,3 +3,6 @@
 #chmod +x /home/ubuntu/server/* war
 #chmod +x /home/ubuntu/server/start_server.sh
 #chmod +x /home/ubuntu/server/stop_server.sh
+
+
+sudo systemctl daemon-reload
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,5 @@
 #catalina.sh start
 ##cp demo-0.0.1-SNAPSHOT.jar /home/ubuntu/ubuntu
 ##cd /home/ubuntu/unbutu
-nohup java -jar /server/ROOT.jar &
-exit 0
+sudo systemctl start rebootServer
+#sudo systemctl stop rebootServer
